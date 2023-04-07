bc5e553639111f6477bca158120b2ea310a7961a
bartwisniewski <bartekwisnia@op.pl>
2023-04-07 21:09:34 +0200

    scrap api view tests

--- source/vpscrapproject/webscrapper/tests/views/test_scrap_api_view.py
+++ source/vpscrapproject/webscrapper/tests/views/test_scrap_api_view.py
@@ -1,13 +1,22 @@
+from datetime import datetime
+from dataclasses import dataclass
+
 from django.test import TestCase, Client
 from http import HTTPStatus
 from django.urls import reverse
 from unittest.mock import patch
+from webscrapper.schemas.schemas import Query
+from webscrapper.schemas.serializers import QuerySerializer
 
 TASK_ID = "abcd1234"
 
 
 def mock_scrap_job(*args, **kwargs):
-    return TASK_ID
+    @dataclass
+    class Task:
+        id: str = TASK_ID
+
+    return Task()
 
 
 class ScrapApiViewTest(TestCase):
@@ -15,17 +24,29 @@ class ScrapApiViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
+        query = Query(
+            region="Mazury",
+            adults=4,
+            children=0,
+            infants=0,
+            start_date=datetime(2023, 7, 20),
+            end_date=datetime(2023, 7, 27),
+        )
+        serializer = QuerySerializer(query)
+        self.query_data = serializer.data
 
-    @patch("scrap.delay", mock_scrap_job)
+    @patch("webscrapper.queue_logic.tasks.scrap.delay", mock_scrap_job)
     def test_should_return_ok(self):
-        response = self.client.get(self.url)
+        response = self.client.post(self.url, json=self.query_data)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    @patch("webscrapper.queue_logic.tasks.scrap.delay", mock_scrap_job)
     def test_should_return_task_id(self):
-        response = self.client.get(self.url)
+        response = self.client.post(self.url, json=self.query_data)
         self.assertIn("task_id", response.json())
 
+    @patch("webscrapper.queue_logic.tasks.scrap.delay", mock_scrap_job)
     def test_should_return_correct_task_id(self):
-        response = self.client.get(self.url)
+        response = self.client.post(self.url, json=self.query_data)
         task_id = response.json().get("task_id", 0)
         self.assertEqual(task_id, TASK_ID)
